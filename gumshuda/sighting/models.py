af387783c252032c7e179239c1d63ba35f7150b6
rohit jhunjhunwla <madhu@rohits-MacBook-Pro.local>
2015-08-18 22:19:41 +0200

    updated

--- gumshuda/sighting/models.py
+++ gumshuda/sighting/models.py
@@ -4,8 +4,8 @@ from datetime import datetime
 # Create your models here.
 
 class sighting( models.Model ):
-	loc = models.CharField( max_length=64 )
-	picture_id = models.ForeignKey('picture')
+    loc = models.CharField( max_length=64 )
+    picture_id = models.ForeignKey('picture')
 
 class people( models.Model ):
     name = models.CharField( max_length=256)
@@ -16,15 +16,15 @@ class people( models.Model ):
     pub_date = models.DateTimeField()
 
 class picture( models.Model ):
-	date = models.DateTimeField(default=datetime.now)
-	data = models.BinaryField()
-	csum = models.CharField( max_length=1024)
+    date = models.DateTimeField(default=datetime.now)
+    data = models.BinaryField()
+    csum = models.CharField( max_length=1024)
 
 class source_picture( models.Model ):
-	people_id = models.ForeignKey('people')
-	picture_id = models.ForeignKey('picture')	
+    people_id = models.ForeignKey('people')
+    picture_id = models.ForeignKey('picture')    
 
 class reported_sighting( models.Model ):
-	picture_id = models.ForeignKey('picture')
-	people_id = models.ForeignKey('people')
-	action = models.TextField( )
+    picture_id = models.ForeignKey('picture')
+    people_id = models.ForeignKey('people')
+    action = models.TextField( )
